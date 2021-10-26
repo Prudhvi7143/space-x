c37954e8fa27380976fa73ae839331c503e0531b
prudhvi reddy <prudhvireddy2014@gmail.com>
2021-10-27 03:23:01 +0530

    Display orginal launches

--- src/components/Launch/Launch.jsx
+++ src/components/Launch/Launch.jsx
@@ -5,7 +5,7 @@ export class Launch extends React.Component{
     render(){
         return(
             <div className = "launch">
-              <img src = {this.props.banner} />
+              <img alt = "" src = {this.props.banner} />
               <div className="content">
                 <div className="info">
                   <h1>{this.props.title}</h1>
--- src/components/LaunchList/LaunchList.jsx
+++ src/components/LaunchList/LaunchList.jsx
@@ -1,29 +1,47 @@
 import React from "react";
 import { Launch } from "../Launch/Launch";
 import "./style.css";
+import axios from "axios";
 
 export class LaunchList extends React.Component {
+
+    state = {
+        launches : []
+    }
+    componentDidMount =() => {
+        this.getLaunches()
+    }
+
+    getLaunches = () => {
+        axios.get('https://api.spacexdata.com/v3/launches').then((response) => {
+            this.setState(
+                { launches: response.data}
+            )
+        }).catch((error) => {
+            console.log('error')
+        })
+    }
+
+    launchlist = () => {
+        const launchlistComponents = this.state.launches.map((launch,index) => {
+            const image = launch.links.flickr_images.length === 0 ?
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQspY_ZuRfOOUv5w04BjNUE9xqGXbNfr12MKA&usqp=CAU' : launch.links.flickr_images[0];
+            console.log(launch.links.flickr_images[0])
+            return <Launch 
+                    key = {"launch_"+index}
+                    banner = {image}
+                    title = {launch.mission_name}
+                    LaunchDate = {launch.launch_date_local}
+                    discription = {launch.details}
+            />
+        })
+        return launchlistComponents;
+    } 
     render() {
+        console.log(this.state.launches)
         return(
             <div className = "LaunchList">
-                <Launch 
-                    banner = "https://farm9.staticflickr.com/8617/16789019815_f99a165dc5_o.jpg"
-                    title = "FalconSat"
-                    LaunchDate = "2006-03-25T10:30:00+12:00"
-                    discription = "CRS-1 successful, but the secondary payload was inserted into abnormally low orbit and lost 
-                                   due to Falcon 9 boost stage engine failure, ISS visiting vehicle safety rules, and the primary 
-                                 payload owner's contractual right to decline a second ignition of the second stage under some conditions."
-                />
-
-                <Launch 
-                    banner = "https://farm8.staticflickr.com/7281/16668845950_7680146525_o.jpg"
-                    title = "DemoSat"
-                    LaunchDate = "2007-03-21T13:10:00+12:00"
-                    discription = "Following second-stage separation, SpaceX conducted a second controlled-descent test of the discarded booster 
-                                   vehicle and achieved the first successful controlled ocean touchdown of a liquid-rocket-engine orbital booster. Following 
-                                   touchdown the first stage tipped over as expected and was destroyed. This was the first Falcon 9 booster to fly with 
-                                   extensible landing legs and the first Dragon mission with the Falcon 9 v1.1 launch vehicle."
-                />
+                {this.launchlist()}
             </div>
         )
     }
