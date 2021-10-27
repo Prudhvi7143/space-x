40ee9164b7707f34f36d18e5ae115c8c3ea8ba4b
prudhvi reddy <prudhvireddy2014@gmail.com>
2021-10-27 23:00:47 +0530

    Add basic Routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,27 @@ import { Header } from "./components/header/Header";
 import "./styles.css";
 //import { Launch } from "./components/Launch/Launch";
 import { LaunchList } from "./components/LaunchList/LaunchList";
+import { LaunchView } from "./components/LaunchView/LaunchView";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <LaunchList />
+      <Router>
+        <Header />
+          <Switch>
+            <Route path="/launch/:flight_number">
+              <LaunchView />
+            </Route>
+            <Route path="/">
+              <LaunchList />
+            </Route>
+          </Switch>
+        </Router>
     </div> 
   );
 }
--- src/components/LaunchList/LaunchList.jsx
+++ src/components/LaunchList/LaunchList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Launch } from "../Launch/Launch";
 import "./style.css";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export class LaunchList extends React.Component {
 
@@ -27,13 +28,16 @@ export class LaunchList extends React.Component {
             const image = launch.links.flickr_images.length === 0 ?
                 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQspY_ZuRfOOUv5w04BjNUE9xqGXbNfr12MKA&usqp=CAU' : launch.links.flickr_images[0];
             console.log(launch.links.flickr_images[0])
-            return <Launch 
-                    key = {"launch_"+index}
-                    banner = {image}
-                    title = {launch.mission_name}
-                    LaunchDate = {launch.launch_date_local}
-                    discription = {launch.details}
-            />
+            return (
+                <Link key = {"launch_"+index} to = {"/launch/"+launch.flight_number}>
+                    <Launch 
+                        banner = {image}
+                        title = {launch.mission_name}
+                        LaunchDate = {launch.launch_date_local}
+                        discription = {launch.details}
+                    />
+                </Link>
+            )
         })
         return launchlistComponents;
     } 
