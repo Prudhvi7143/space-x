fdce3baac5b701e03552ae48b5709cbc5528eed1
prudhvi reddy <prudhvireddy2014@gmail.com>
2021-10-28 20:43:51 +0530

    Display lounch based on router

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Header } from "./components/header/Header";
 import "./styles.css";
 //import { Launch } from "./components/Launch/Launch";
 import { LaunchList } from "./components/LaunchList/LaunchList";
-import { LaunchView } from "./components/LaunchView/LaunchView";
+import  LaunchView  from "./components/LaunchView/LaunchView";
 
 import {
   BrowserRouter as Router,
